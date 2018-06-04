66517f6c9ad3ba95aa1502b086a6aa94a28481e1
johans <duanqingbb@163.com>
2018-06-04 03:41:41 +0000

    --allow-empty-message

--- src/Pollen/Oscillate.php
+++ src/Pollen/Oscillate.php
@@ -1,5 +1,14 @@
 <?php
 namespace Twig\Pollen;
 
-class OscillateClient implements Client {
+use Twig\Pollen\Osci_v1 as Osci;
+
+class Oscillate extends Osci  implements Client {
+
+    public function get_url() {
+    }
+    public function get_status() {
+    }
+    public function get_url_filter() {
+    }
 }
