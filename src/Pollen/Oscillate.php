4f3e9a2edb6d16d0404b25a5b6366198e1ca4f9f
johans <duanqingbb@163.com>
2018-06-05 10:55:41 +0000

    Add Oscillate client

--- src/Pollen/Oscillate.php
+++ src/Pollen/Oscillate.php
@@ -1,14 +1,41 @@
 <?php
 namespace Twig\Pollen;
 
-use Twig\Pollen\Osci_v1 as Osci;
+use Twig\Pollen\Protocols\Osci_v1 as Osci;
+use Twig\Pollen\Client;
+use Twig\Pollen\Socket;
 
 class Oscillate extends Osci  implements Client {
+    private $socket;
+
+    public function __construct($address,$port) {
+        $this->socket = new Socket($address,$port);
+    }
+
+    public function status() {
+        $protocol = self::NAME.'/'.self::VERSION.' STATUS';
+        $this->socket->write($protocol,strlen($protocol));
+        $response = $this->socket->read();
+        return $response;
+    }
 
     public function get_url() {
+        $protocol = self::NAME.'/'.self::VERSION.' GET_URL';
+        $this->socket->write($protocol,strlen($protocol));
+        $response = $this->socket->read();
+        return trim($response);
     }
-    public function get_status() {
+    public function get_url_filter($domain) {
+        $protocol = self::NAME.'/'.self::VERSION.' GET_URL_FILTER '.$domain;
+        $this->socket->write($protocol,strlen($protocol));
+        $response =  $this->socket->read();
+        return $response;
     }
-    public function get_url_filter() {
+
+    public function put_url($url) {
+        $protocol = self::NAME.'/'.self::VERSION.' PUT_URL '.$url;
+        $this->socket->write($protocol,strlen($protocol));
+        $response =  $this->socket->read();
+        return $response;
     }
 }
---
+++ src/Pollen/Socket.php
@@ -0,0 +1,44 @@
+<?php
+namespace Twig\Pollen;
+
+use Twig\Pollen\Protocols\Sockets;
+
+class Socket extends Sockets {
+    private $address;
+    private $port;
+    private $socket;
+    private $options = [
+        'domain'=>   AF_INET,
+        'type'  =>   SOCK_STREAM,
+        'protocol'=> SOL_TCP,
+        'block' => false
+    ]; 
+
+    public function __construct($address,$port,$socket_options = []) {
+        $this->address = $address;
+        $this->port    = $port;
+        if(empty($options)) {
+            $options = $this->options;
+        } else {
+            $options = array_merge($this->options,$socket_options);
+        }
+        $this->socket = socket_create($options['domain'],$options['type'],$options['protocol']);
+    }
+
+    public function read(int $size = 1024) {
+        $data = '';
+        while($content =  socket_read($this->socket,$size)) {
+            $data.=$content;
+        }
+        return $data;
+    }
+
+    public function write(string $content, int $size) {
+        socket_connect($this->socket,$this->address,$this->port);
+        $size = socket_write($this->socket,$content,$size);
+    }
+
+    public function close() {
+        socket_close($this->socket);
+    }
+}
