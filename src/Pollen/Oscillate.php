9ba32dd3532a65066ba2b57d11ae19f0f1862044
johans <duanqiaobb@163.com>
2018-06-06 14:02:12 +0000

    --allow-empty-message

--- src/Pollen/Oscillate.php
+++ src/Pollen/Oscillate.php
@@ -2,55 +2,7 @@
 namespace Twig\Pollen;
 use Twig\Pollen\Socket;
 use Twig\Pollen\Protocols\Osci_v1 as Osci;
-
-<<<<<<< HEAD
-class Oscillate extends Osci implements Client {
-    private $socket;
-
-    public function __construct($address,$port) {
-        $this->socket = new Socket($address,$port);
-    }
-    /** 
-     * @method status()
-     * get status of crawlers from host
-     */
-    public function status() {
-        $status_proto = 'OSCI/1.0 STATUS';
-        $this->socket->write($status_proto,strlen($status_proto));
-        $status = $this->socket->read();
-        return $status;
-    }
-    /**
-     * @method url()
-     * get url from host
-     */
-    public function url() {
-        return $this->get_url();
-    }
-
-    /**
-     * @method url_filter()
-     * get url filtered by domain from host
-     */
-    public function url_filter($domain) {
-        return $this->get_url_filter($domain);
-    }
-
-    public function get_url() {
-        $proto = 'OSCI/1.0 GET_URL';
-        $this->socket->write($proto,strlen($proto));
-        $url = $this->socket->read();
-        return $url;
-    }
-    public function get_url_filter($domain) {
-        $proto = 'OSCI/1.0 GET_URL_FILTER';
-        $this->socket->write($proto,strlen($proto));
-        $url = $this->socket->read();
-        return $url;
-=======
-use Twig\Pollen\Protocols\Osci_v1 as Osci;
 use Twig\Pollen\Client;
-use Twig\Pollen\Socket;
 
 class Oscillate extends Osci  implements Client {
     private $socket;
@@ -84,6 +36,5 @@ class Oscillate extends Osci  implements Client {
         $this->socket->write($protocol,strlen($protocol));
         $response =  $this->socket->read();
         return $response;
->>>>>>> 1bc8ec4b2d2a40c4b1631394172d0c3c9e3f21f4
     }
 }
--- src/Pollen/Socket.php
+++ src/Pollen/Socket.php
@@ -1,45 +1,9 @@
 <?php
 namespace Twig\Pollen;
-<<<<<<< HEAD
 use Twig\Pollen\Client;
 use Twig\Pollen\Protocols\Sockets;
-use Twig\Event\Dispatcher as Dispatcher;
-use Twig\Event\Listener as Listener;
-use Twig\Event\Event as Event;
 
-/**
- * class Socket
- *
- * Socket client
- *
- */
-class Socket extends Sockets implements Client {
-    private $socket;
-    private $event = [
-    ];
-    public function __construct($address, $port, $socket_option = []) {
-        if (empty($socket_option)) {
-            $this->socket = socket_create(AF_INET,SOCK_STREAM,SOL_TCP);
-        }
-        $this->socket = socket_create(AF_INET,SOCK_STREAM,SOL_TCP);
-        socket_connect($this->socket,$address,$port);
-    }
-
-    public function write(string $write, int $length) {
-        return socket_write($this->socket, $write, $length);
-    }
-
-    public function read() {
-        $revdata = [];
-        while($rdata = socket_read($this->socket,1024,PHP_BINARY_READ)) {
-            $revdata[] = $rdata;
-        }
-        return $revdata;
-=======
-
-use Twig\Pollen\Protocols\Sockets;
-
-class Socket extends Sockets {
+class Socket extends Sockets implements Client{
     private $address;
     private $port;
     private $socket;
@@ -76,6 +40,5 @@ class Socket extends Sockets {
 
     public function close() {
         socket_close($this->socket);
->>>>>>> 1bc8ec4b2d2a40c4b1631394172d0c3c9e3f21f4
     }
 }
